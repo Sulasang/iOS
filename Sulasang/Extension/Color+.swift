ecd649543b50a9bdb37cecf8d9d8e0cdbcaddc9f
sozohoy <sozohoy@gmail.com>
2023-09-10 03:23:29 +0900

    feat: extension

--- Sulasang/Extension/Color+.swift
+++ Sulasang/Extension/Color+.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+import SwiftUI
+
+extension Color {
+    
+    static let mainColor = Color(red: 19 / 255, green: 46 / 255, blue: 106 / 255)
+    static let secondColor = Color(red: 234 / 255, green: 181 / 255, blue: 70 / 255)
+    
+}
