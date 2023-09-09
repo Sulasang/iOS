989176e971183a0e914c40e1c7527dd2fee69f22
sozohoy <sozohoy@gmail.com>
2023-09-10 00:22:42 +0900

    init

---
+++ Sulasang/SulasangApp.swift
@@ -0,0 +1,17 @@
+//
+//  SulasangApp.swift
+//  Sulasang
+//
+//  Created by 한지석 on 2023/09/10.
+//
+
+import SwiftUI
+
+@main
+struct SulasangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
