7cbee0c2c2b1264fb787ef3bb6801d005a5b6a7b
sozohoy <sozohoy@gmail.com>
2023-09-10 03:21:38 +0900

    feat: cafeteria & cafeteriaType

--- Sulasang/Model/Cafeteria.swift
+++ Sulasang/Model/Cafeteria.swift
@@ -7,3 +7,15 @@
 
 import Foundation
 
+struct Cafeteria: Identifiable {
+    let id: UUID
+    let type: CafeteriaType
+    let menu: [String]
+}
+
+enum CafeteriaType: String {
+    case chefTable = "Chef Table"
+    case littleKitchen = "Little Kitchen"
+    case momsCook = "Mom's Cook"
+    case staff = "교직원"
+}
