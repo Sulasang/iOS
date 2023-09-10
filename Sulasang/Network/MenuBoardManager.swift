2ff6a5d27a83ceab3896a668975f53d06ab27c7d
sozohoy <sozohoy@gmail.com>
2023-09-11 01:37:18 +0900

    add: MenuBoardManager & Response

---
+++ Sulasang/Network/MenuBoardManager.swift
@@ -0,0 +1,8 @@
+//
+//  MenuBoardManager.swift
+//  Sulasang
+//
+//  Created by 한지석 on 2023/09/10.
+//
+
+import Foundation
---
+++ Sulasang/Network/MenuBoardResponse.swift
@@ -0,0 +1,8 @@
+//
+//  MenuBoardResponse.swift
+//  Sulasang
+//
+//  Created by 한지석 on 2023/09/10.
+//
+
+import Foundation
