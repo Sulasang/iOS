b455d96cecf8b9ab84956db2f6ee8e66078a6c75
sozohoy <sozohoy@gmail.com>
2023-09-11 01:38:58 +0900

    feat: DTO 연결, 데이터 패치

--- Sulasang/Observable/MainObservable.swift
+++ Sulasang/Observable/MainObservable.swift
@@ -8,10 +8,37 @@
 import Foundation
 
 final class MainObservable: ObservableObject {
-    @Published var cafeteria: [Cafeteria] = [
-        Cafeteria(id: UUID(), type: .chefTable, menu: ["(셀프)치킨샐러드"]),
-        Cafeteria(id: UUID(), type: .littleKitchen, menu: ["밥", "김", "김치", "소세지"]),
-        Cafeteria(id: UUID(), type: .MomsCook, menu: ["밥", "김", "김치", "소세지"]),
-        Cafeteria(id: UUID(), type: .staff, menu: ["밥", "김", "김치", "소세지", "밥", "김", "김치", "소세지"])
-    ]
+    
+    let menuBoardManager = MenuBoardManager()
+    @Published var cafeteria: [Cafeteria] = []
+    var today = ""
+    
+    init() {
+        self.fetchData()
+        self.today = self.getCurrentFormattedDateString()
+    }
+    
+    func fetchData() {
+        menuBoardManager.fetchData { result in
+            switch result {
+            case .success(let result):
+                let fetchMenu = result.map {
+                    Cafeteria(id: UUID(), type: CafeteriaType(rawValue: $0.company) ?? .staff, menu: $0.mainMenu)
+                }
+                DispatchQueue.main.async {
+                    self.cafeteria = fetchMenu
+                }
+            case .failure(let error):
+                print("@Log Observable - \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    func getCurrentFormattedDateString() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM월 dd일 (E)"
+        let currentDate = Date()
+        return dateFormatter.string(from: currentDate)
+    }
+    
 }
